2063e9278baf295aee7063fd9f19245378dc1e96
Vsevolod Danilov <vsevolod.dnlv@mail.ru>
2024-05-20 13:34:45 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Список сотрудников</h1>
+    <table>
+      <thead>
+      </thead>
+      <tbody> 
+        <data-row 
+          v-for="employee in employees"
+          :key="employee.name" 
+          :employee="employee"
+        />
+      </tbody> 
+    </table> 
+  </div> 
+</template>
+
+<script> 
+import DataRow from './components/DataRow.vue'
+import { employees } from './data/employees.js'
+
+export default {
+  components: { DataRow },
+  data() { 
+    return { employees: employees }; 
+  }
+}; 
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataRow.vue
@@ -0,0 +1,49 @@
+<template>
+  <tr>
+    <td>{{ employee.name }}</td>
+    <td>{{ formatGender(employee.sex) }}</td>
+    <td>{{ formatDate(employee.birth_date) }}</td>
+    <td>{{ employee.organisation }}</td>
+    <td>{{ employee.job_title }}</td>
+    <td>
+      <span v-if="employee.fired">уволен</span>
+    </td>
+  </tr>
+</template>
+
+<script>
+import dayjs from 'dayjs';
+
+export default {
+  props: {
+    employee: Object
+  },
+  methods: {
+    formatDate(value) {
+      const date = dayjs.unix(value);
+      return date.format('DD.MM.YYYY');
+    },
+    formatGender(gender) {
+      return gender ? 'женский' : 'мужской';
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
